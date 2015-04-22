d3bc9c62ec67ab5c8d6e580142c034bbe6aab53f
srothman <srothman@mcdpartners.com>
2015-04-22 17:43:16 -0400

    Switching to Mocha testing suite

--- test/e2e/file_created.js
+++
@@ -1,8 +0,0 @@
-casper.test.begin('Successful Submit', 1, function suite(test) {
-  casper.start('index.js', function() {
-    console.log('test started');
-  });
-  casper.run(function() {
-    test.done();
-  });
-});
